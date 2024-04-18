16b82988e624a629d15f823fd7a3932d6277983f
D Rao <tdayakarrao@gmail.com>
2024-04-18 23:26:02 +0530

    Hello Spring and Microservices World

---
+++ Hello World/src/welcome/Hello_Spring_Microservices_World.java
@@ -0,0 +1,11 @@
+package welcome;
+
+public class Hello_Spring_Microservices_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcomr to the World of Java Spring and Microservices");  
+		
+	}
+
+}
