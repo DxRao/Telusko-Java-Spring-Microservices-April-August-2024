e69c520108493f80913d8ebee1659ae27d562e2a
D Rao <tdayakarrao@gmail.com>
2024-04-28 01:09:03 +0530

    Spring Core-05: Autowiring through Xml config file

--- 06-SpringCore-05_AutowireThroughXmlConfigFile/src/main/java/com/telusko/main/LaunchApp.java
+++ 06-SpringCore-05_AutowireThroughXmlConfigFile/src/main/java/com/telusko/main/LaunchApp.java
@@ -3,8 +3,7 @@ package com.telusko.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class LaunchApp 
-{
+public class LaunchApp {
 
 	public static void main(String[] args) 
 	{
